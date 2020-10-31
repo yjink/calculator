0043e579f139af42d444b18108f4561f7809b64a
yjink <kweonyujin@gmail.com>
2020-10-31 12:54:28 -0400

    Create basic operator functions

---
+++ script.js
@@ -0,0 +1,37 @@
+// Operate!
+function operate(a, operate, b) {
+    let total = 0;
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+        if (operate === "+") {
+            total = add(a, b);
+        } else if (operate === "-") {
+            total = subtract(a, b);
+        } else if (operate === "*") {
+            total = multiply(a, b);
+        } else if (operate === "/") {
+            total = divide(a, b);
+        }
+    return total;
+    } else {
+        return 0;
+    }
+}
+
+
+// Basic Math Operators
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
