cc4b086e6a916fd222ffbd3ead771e1e0e72c537
yjink <kweonyujin@gmail.com>
2021-01-02 19:50:41 -0500

    Removed keyboard function temporarily

--- script.js
+++ script.js
@@ -10,43 +10,43 @@ let total = '';
 
 // KEYBOARD COMPATIBILITY
 
-body.addEventListener('keydown', event => {
-    let cKey = '1234567890/*-+.';
-    // cKey are keys on a calculator
+// body.addEventListener('keydown', event => {
+//     let cKey = '1234567890/*-+.';
+//     // cKey are keys on a calculator
 
-    if (cKey.includes(event.key) && display.join('').length < 14) {
-        let keyVal = event.key;
+//     if (cKey.includes(event.key) && display.join('').length < 14) {
+//         let keyVal = event.key;
 
-        if (total === display[0] && (!display[1])) {
-            if (Number.isInteger(Number(keyVal)) || keyVal === '.') {
-                clearDisplay();
-            }
-        }
+//         if (total === display[0] && (!display[1])) {
+//             if (Number.isInteger(Number(keyVal)) || keyVal === '.') {
+//                 clearDisplay();
+//             }
+//         }
 
-        display.push(keyVal);
-    } else if (event.key === 'Enter') {
-        mathEquals();
-    } else if (event.key === 'Backspace') {
-        back();  
-    } else {
-        display.push(keyVal);
-    }
+//         display.push(keyVal);
+//     } else if (event.key === 'Enter') {
+//         mathEquals();
+//     } else if (event.key === 'Backspace') {
+//         back();  
+//     } else {
+//         display.push(keyVal);
+//     }
 
-    display = cleanDisplay(display);
+//     display = cleanDisplay(display);
 
-    let displayP = document.createElement('p');
-    let displayText = document.createTextNode(display.join(''));
+//     let displayP = document.createElement('p');
+//     let displayText = document.createTextNode(display.join(''));
 
-    displayP.appendChild(displayText);
+//     displayP.appendChild(displayText);
 
-    while (screen.lastChild) {
-        screen.removeChild(screen.lastChild);
-    }
+//     while (screen.lastChild) {
+//         screen.removeChild(screen.lastChild);
+//     }
 
-    screen.appendChild(displayP);
+//     screen.appendChild(displayP);
 
 
-})
+// })
 
 
 // THE MECHANICS
